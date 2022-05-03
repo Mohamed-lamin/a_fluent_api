fc496fc03a24e598d6499af757aa0dd0d5f17004
Damin <daminhaidar08@gmail.com>
2022-05-03 15:48:41 +0200

    testing 1:N

---
+++ test-migration/Data/Data.cs
@@ -0,0 +1,25 @@
+﻿namespace CreateAminimalAPI.Data;
+
+
+public class User
+{
+    public int ID { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public List<Toy> Toys { get; set; } = new();
+
+
+}
+
+public class Toy
+{
+    public int ID { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public User? User { get; set; }
+
+    public int UserID { get; set; }
+}
+
---
+++ test-migration/Data/DataDbContext.cs
@@ -0,0 +1,22 @@
+﻿namespace CreateAminimalAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+
+
+public class DataDbContext:DbContext
+{
+
+    public DataDbContext(DbContextOptions<DataDbContext> options):
+
+        base(options)
+    {
+        
+    }
+
+    public DbSet<User> Users => Set<User>();
+
+    public DbSet<Toy> Toys => Set<Toy>();
+
+}
+
